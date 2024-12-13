fcdbd4e261f2d016abd0d1fc2d2e0b59e71b6e4b
sunsitox <67767487+sunsitox@users.noreply.github.com>
2024-12-12 23:58:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const filePath = path.join(__dirname, "Data.json");
 server.use(
   cors({
     origin: [
-      "https://proyecto-mobil-entrega-3.onrender.com", // Tu dominio en Render
+      "https://proyecto-mobil-entrega-3-1.onrender.com/", // Tu dominio en Render
       "http://localhost:8100", // Si usas Ionic serve localmente
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],

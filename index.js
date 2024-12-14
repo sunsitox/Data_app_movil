902a3100dd4346459cf793ea0142b7589b1d9ad2
sunsitox <67767487+sunsitox@users.noreply.github.com>
2024-12-14 06:54:09 -0300

    test envio

--- index.js
+++ index.js
@@ -5,6 +5,57 @@ const middlewares = jsonServer.defaults();
 const enviarCorreo = require("./mailer");
 const port = process.env.PORT || 10000;
 
+//..
+
+// Ruta para solicitud de recuperación de contraseña
+server.post("/passwordResetRequest", async (req, res) => {
+  const email = req.body?.email;
+
+  if (!email) {
+    return res.status(400).json({ error: "Correo electrónico requerido." });
+  }
+
+  // Generar un token único
+  const token = Math.random().toString(36).substr(2);
+  const resetLink = `http://localhost:8100/reset-password?token=${token}`;
+
+  // Configurar el correo
+  const asunto = "Recuperación de Contraseña";
+  const mensaje = `
+    <h1>Recuperación de Contraseña</h1>
+    <p>Haz clic en el siguiente enlace para restablecer tu contraseña:</p>
+    <a href="${resetLink}">Restablecer Contraseña</a>`
+  ;
+
+  const requestData = {
+    email: email,
+    token: token,
+    isValid: true,
+  };
+
+  try {
+    // Enviar el correo
+    await enviarCorreo(email, asunto, mensaje);
+
+    // Guardar los datos en el archivo JSON
+    await guardarDatos(requestData);
+
+    res.status(200).json({
+      message: "Correo enviado con éxito y solicitud registrada.",
+      token: token,
+    });
+  } catch (error) {
+    if (error.message === "Ya existe una solicitud activa para este correo.") {
+      res.status(409).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "Error interno al procesar la solicitud." });
+    }
+  }
+});
+
+
+//..
+
 server.use(middlewares);
 server.use(router);
 
